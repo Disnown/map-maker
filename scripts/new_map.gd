4a8d206749d8c799248945740da762863047adec
Disnown <hasanrehan210@gmail.com>
2026-07-22 13:21:45 +0800

    ghost tile added

--- scripts/new_map.gd
+++ scripts/new_map.gd
@@ -7,11 +7,18 @@ var mesh_library : MeshLibrary
 
 var selected_tile = ""
 
+var current_layer := 0
+
+@onready var ghost_tile: MeshInstance3D = $GhostTile
+var selected_tile_id := -1
+
 func _ready():
 
 	mesh_library = grid_map.mesh_library
 
 	create_tile_buttons()
+
+	ghost_tile.visible = false
 
 func create_tile_buttons():
 
@@ -27,12 +34,69 @@ func create_tile_buttons():
 
 		tile_list.add_child(button)
 
-		var texture = load("res://assets/previews/" + button.text + ".png")
+		var preview_path: String = "res://assets/previews/" + button.text + ".png"
 
-		button.icon = texture
+		if ResourceLoader.exists(preview_path):
+			button.icon = load(preview_path)
 
 func _on_tile_selected(id:int):
 
+	selected_tile_id = id
+
 	selected_tile = mesh_library.get_item_name(id)
 
-	print("Selected:", selected_tile)
+	print("Selected:", mesh_library.get_item_name(id))
+
+	update_ghost_mesh()
+
+func update_ghost_mesh():
+
+	if selected_tile_id == -1:
+		ghost_tile.visible = false
+		return
+
+
+	var mesh = mesh_library.get_item_mesh(selected_tile_id)
+
+	ghost_tile.mesh = mesh
+
+	ghost_tile.visible = true
+
+func _process(delta):
+
+	if selected_tile_id == -1:
+		return
+
+	update_ghost_position()
+
+func update_ghost_position():
+
+	var mouse = get_viewport().get_mouse_position()
+
+	var ray_origin = camera.project_ray_origin(mouse)
+
+	var ray_direction = camera.project_ray_normal(mouse)
+
+
+	var plane = Plane(
+		Vector3.UP,
+		0
+	)
+
+
+	var hit = plane.intersects_ray(
+		ray_origin,
+		ray_direction
+	)
+
+
+	if hit == null:
+		return
+
+
+	var cell = grid_map.local_to_map(
+		grid_map.to_local(hit)
+	)
+
+
+	ghost_tile.position = grid_map.map_to_local(cell)
