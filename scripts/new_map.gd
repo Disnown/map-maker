24af2724ee836f08dfd5b6ab334c96fa6494e26b
Disnown <hasanrehan210@gmail.com>
2026-07-22 14:17:14 +0800

    delete function working

--- scripts/new_map.gd
+++ scripts/new_map.gd
@@ -12,13 +12,47 @@ var current_layer := 0
 @onready var ghost_tile: MeshInstance3D = $GhostTile
 var selected_tile_id := -1
 
+@onready var delete_ghost: MeshInstance3D = $DeleteGhostTile
+
+@onready var create_button = $CanvasLayer/Toolbar/ScrollContainer/TileList/CreateButton
+@onready var delete_button = $CanvasLayer/Toolbar/ScrollContainer/TileList/DeleteButton
+
+enum Tool {
+	PLACE,
+	DELETE
+}
+
+var current_tool = Tool.PLACE
+
 func _ready():
 
 	mesh_library = grid_map.mesh_library
 
+	create_button.pressed.connect(_on_place_button_pressed)
+	delete_button.pressed.connect(_on_delete_button_pressed)
+
 	create_tile_buttons()
 
 	ghost_tile.visible = false
+
+func _on_place_button_pressed():
+
+	current_tool = Tool.PLACE
+
+	delete_ghost.visible = false
+	if selected_tile_id != -1:
+		ghost_tile.visible = true
+
+	print("Tool: Place")
+
+func _on_delete_button_pressed():
+
+	current_tool = Tool.DELETE
+
+	ghost_tile.visible = false
+	delete_ghost.visible = true
+
+	print("Tool: Delete")
 
 func create_tile_buttons():
 
@@ -64,10 +98,15 @@ func update_ghost_mesh():
 
 func _process(delta):
 
-	if selected_tile_id == -1:
-		return
+	if current_tool == Tool.PLACE:
 
-	update_ghost_position()
+		if selected_tile_id != -1:
+			update_ghost_position()
+
+
+	elif current_tool == Tool.DELETE:
+
+		update_delete_preview()
 
 func update_ghost_position():
 
@@ -106,7 +145,14 @@ func _unhandled_input(event):
 
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 
-			place_tile()
+			match current_tool:
+
+				Tool.PLACE:
+					place_tile()
+
+				Tool.DELETE:
+					remove_tile()
+
 
 func place_tile():
 
@@ -154,3 +200,60 @@ func get_placement_cell() -> Vector3i:
 
 
 	return cell
+
+func remove_tile():
+
+	var cell = get_mouse_cell()
+
+
+	if cell == Vector3i(-999,-999,-999):
+		return
+
+
+	var tile_id = grid_map.get_cell_item(cell)
+
+
+	if tile_id == -1:
+		print("No tile here")
+		return
+
+
+	grid_map.set_cell_item(
+		cell,
+		-1
+	)
+
+
+	print("Removed tile at:", cell)
+
+func get_mouse_cell() -> Vector3i:
+
+	var mouse = get_viewport().get_mouse_position()
+
+	var ray_origin = camera.project_ray_origin(mouse)
+	var ray_direction = camera.project_ray_normal(mouse)
+
+
+	var plane = Plane(
+		Vector3.UP,
+		0
+	)
+
+
+	var hit = plane.intersects_ray(
+		ray_origin,
+		ray_direction
+	)
+
+
+	if hit == null:
+		return Vector3i(-999,-999,-999)
+
+
+	return grid_map.local_to_map(
+		grid_map.to_local(hit)
+	)
+	
+	
+	
+	
